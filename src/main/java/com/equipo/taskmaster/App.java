13e94344e497a1532bedc80841dbfa67e389e321
Fabián Hernández <fahg76@gmail.com>
2025-06-17 23:05:55 -0400

    Último commit por uso de Visual Studio Code

--- src/main/java/com/equipo/taskmaster/App.java
+++ src/main/java/com/equipo/taskmaster/App.java
@@ -2,17 +2,21 @@ package com.equipo.taskmaster;
 
 import java.util.ArrayList;
 
+
 public class App {
     public static ArrayList<String> tasks = new
 ArrayList<>();
 
-    public static void main(String[] args) {
-        System.out.println("Ambiente: " + System.getProperty("env.name"));
+    
+    public static void main(String[] args) {        
         System.out.println("Bienvenido a TaskMaster!");
+        System.out.println("Ambiente: " + System.getProperty("env.name"));
         addTask("Estudiar Maven");
         addTask("Leer sobre CI/CD");
         printTasks();
     }
+
+
 
     public static void addTask(String task) {
         tasks.add(task);
